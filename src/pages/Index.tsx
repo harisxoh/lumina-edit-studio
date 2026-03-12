a63488a4d6adaea093eeddf4f3710cf63cac68e8
[dyad] <git@dyad.sh>
2026-03-12 14:00:40 +0530

    [dyad] Added a particle background using react-particles-js. The particles are semi-transparent and responsive, creating a subtle aesthetic effect without overpowering the content. - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,27 +1,30 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import GallerySection from "@/components/GallerySection";
-import ServicesSection from "@/components/ServicesSection";
-import PricingSection from "@/components/PricingSection";
-import SupportSection from "@/components/SupportSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
-import ContactSection from "@/components/ContactSection";
-import Footer from "@/components/Footer";
+import React from "react";  
+import Navbar from "@/components/Navbar";  
+import HeroSection from "@/components/HeroSection";  
+import GallerySection from "@/components/GallerySection";  
+import ServicesSection from "@/components/ServicesSection";  
+import PricingSection from "@/components/PricingSection";  
+import SupportSection from "@/components/SupportSection";  
+import TestimonialsSection from "@/components/TestimonialsSection";  
+import ContactSection from "@/components/ContactSection";  
+import Footer from "@/components/Footer";  
+import ParticlesComponent from "@/components/Particles";  
 
-const Index = () => {
-  return (
-    <div className="min-h-screen bg-background">
-      <Navbar />
-      <HeroSection />
-      <GallerySection />
-      <ServicesSection />
-      <PricingSection />
-      <SupportSection />
-      <TestimonialsSection />
-      <ContactSection />
-      <Footer />
-    </div>
-  );
-};
+const Index = () => {  
+  return (  
+    <div className="min-h-screen bg-background">  
+      <ParticlesComponent /> {/* Add particles as background */}  
+      <Navbar />  
+      <HeroSection />  
+      <GallerySection />  
+      <ServicesSection />  
+      <PricingSection />  
+      <SupportSection />  
+      <TestimonialsSection />  
+      <ContactSection />  
+      <Footer />  
+    </div>  
+  );  
+};  
 
 export default Index;
